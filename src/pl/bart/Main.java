5d54156d6dac893499eb2b04e1035d5ae95a05b6
Bart Chw <bartosz.chwialkowski@protonmail.com>
2022-01-10 10:31:12 +0100

    Initial commit

---
+++ src/pl/bart/Main.java
@@ -0,0 +1,32 @@
+package pl.bart;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+        System.out.println("Float minimum value = " + myMinFloatValue);
+        System.out.println("Float maximum value = " + myMaxFloatValue);
+
+        double myMinDoubleValue = Double.MIN_VALUE;
+        double myMaxDoubleValue = Double.MAX_VALUE;
+        System.out.println("Double minimum value = " + myMinDoubleValue);
+        System.out.println("Double maximum value = " + myMaxDoubleValue);
+
+        int myIntValue = 5 / 3;
+        float myFloatValue = 5.f / 3f;
+        double myDoubleValue = 5.d / 3d;
+        System.out.println("MyIntValue = " + myIntValue);
+        System.out.println("MyFloatValue = " + myFloatValue);
+        System.out.println("MyDoubleValue = " + myDoubleValue);
+
+        double myPounds = 5000d;
+        double convertedToKg = myPounds * 0.45359237d;
+        System.out.println("Converted kilograms = " + convertedToKg);
+
+
+
+	// write your code here
+    }
+}
